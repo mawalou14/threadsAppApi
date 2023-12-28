da27dbcc344833d8a6f652bc4836129811b7c143
mawalou14 <awaloum213@gmail.com>
2023-12-28 16:59:20 +0100

    add isnot empty case

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,6 +1,7 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateCommentDto {
     @IsString()
+    @IsNotEmpty()
     name: string;
 }
