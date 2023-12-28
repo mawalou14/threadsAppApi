d04bfb1a61484fb496e0c0b1dd391dd46bda5b21
mawalou14 <awaloum213@gmail.com>
2023-12-28 16:56:28 +0100

    inject comment into service

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Comment } from './schemas/comment.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class CommentsService {
+  constructor(@InjectModel(Comment.name) private commentModel: Model<Comment>) { }
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
