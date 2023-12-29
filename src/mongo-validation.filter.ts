de54050b289dc011f0eb4c8f8185929991dd82b7
mawalou14 <awaloum213@gmail.com>
2023-12-29 22:49:22 +0100

    update error filter

--- src/mongo-validation.filter.ts
+++ src/mongo-validation.filter.ts
@@ -7,12 +7,12 @@ import ValidationError = Error.ValidationError
 export class ValidationErrorFilter implements RpcExceptionFilter {
     catch(exception: ValidationError, host: ArgumentsHost): any {
         const ctx = host.switchToHttp(),
-        response = ctx.getResponse();
+            response = ctx.getResponse();
 
         return response.status(400).json({
             statusCode: 400,
             createBy: 'ValidationErrorFilter',
-            errors: exception.errors
+            errors: exception
         })
     }
 }
